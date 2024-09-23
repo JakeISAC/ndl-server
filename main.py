b083e51fc40ceda8ab6bdc4f6b1f7f443ad58868
JakeISAC <66417893+JakeISAC@users.noreply.github.com>
2024-09-23 10:56:59 +0200

    Add Mikhail's images and tweak face comparison tolerance
    Added new images for Mikhail to the authorized_faces directory. Adjusted the face comparison tolerance from 0.7 to 0.55 for improved matching accuracy. Also updated import statements in main.py for better organization and code maintenance.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import uuid
+
 from core.face_detection import FaceDetection
+from database.db_operations import DatabaseOperations
+from domains.people import Person
+from face_recognition_util.encode_faces import EncodeFaces
+from util.program_codes import AuthorizationStatus
 
 if __name__ == '__main__':
-    # new_encode = EncodeFaces("C:\\Users\\jakub\\PycharmProjects\\ndl\\authorized_faces\\images\\masha")
-    # encode = new_encode.encode_face_file()
-    # person = Person(id=uuid.uuid4(), name="Masha Ushakov",
-    #                 images_path="C:\\Users\\jakub\\PycharmProjects\\ndl\\authorized_faces\\images\\masha",
+    # new_encode = EncodeFaces("C:\\Users\\jakub\\PycharmProjects\\ndl\\authorized_faces\\images\\mikhail")
+    # encode = new_encode.generate_encodings_from_file()
+    # person = Person(id=uuid.uuid4(), name="Mikhail Ushakov",
+    #                 images_path="C:\\Users\\jakub\\PycharmProjects\\ndl\\authorized_faces\\images\\mikhail",
     #                 authorization=AuthorizationStatus.AUTHORIZED, face_encodings=encode)
     # person.add(DatabaseOperations())
     # print(encode)
-
-    # compare_faces = CompareFaces(encode["encodings"], encode["encodings"][0])
-    # print(compare_faces.compare_faces())
 
     face_detection = FaceDetection()
     face_detection.start()
