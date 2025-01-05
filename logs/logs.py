8bcb62703f8c2f83dfdc14ef5f1d5efba0d24935
jakeISAC <kuba.sykulski@proton.me>
2025-01-05 14:46:24 +0100

    Use trace level for logger initialization message
    Changed the log level from `info` to `trace` for the logger initialization message to provide more granular logging. This adjustment ensures finer control over logging verbosity during debugging or development.

--- logs/logs.py
+++ logs/logs.py
@@ -6,5 +6,5 @@ logging.add("/home/ubuntu/ndl/logs/general.log", rotation="100 MB", level="ERROR
 class Logs:
     @classmethod
     def get_logger(cls):
-        logger.info("Logger instance initialized")
+        logger.trace("Logger instance initialized")
         return logging
