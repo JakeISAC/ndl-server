05005530c43093ef6b312bf542ea94bae944ef1b
jakeISAC <kuba.sykulski@proton.me>
2025-01-05 14:53:46 +0100

    Remove unnecessary logger initialization message
    The log message indicating logger instance initialization was removed as it provided no functional value. This change simplifies the logging process and avoids redundant log entries.

--- logs/logs.py
+++ logs/logs.py
@@ -6,5 +6,4 @@ logging.add("/home/ubuntu/ndl/logs/general.log", rotation="100 MB", level="ERROR
 class Logs:
     @classmethod
     def get_logger(cls):
-        logger.info("Logger instance initialized")
         return logging
