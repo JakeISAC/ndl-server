f8065b8f003f137eaacb06f83332667ba43aaf03
jakeISAC <kuba.sykulski@proton.me>
2025-01-05 13:22:35 +0100

    Enable file logging with exception level and rotation
    Uncommented and updated the logger to write logs to a file with a rotation of 100 MB and set the log level to "EXCEPTION". This prepares the system for improved error tracking in deployment environments.

--- logs/logs.py
+++ logs/logs.py
@@ -4,8 +4,7 @@ from loguru import logger
 class Logs:
     def __init__(self):
         self._logger = logger
-        # TODO: Uncomment for final deployment
-        # self._logger.add("/home/ubuntu/ndl/logs/general.log", rotation="100 MB")
+        self._logger.add("/home/ubuntu/ndl/logs/general.log", rotation="100 MB", level="EXCEPTION")
 
     def get_logger(self):
         return self._logger
